55983b8cb9e12605eb3d3f0a94c17a7a94fe56bb
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-09-22 21:55:15 +0200

    Update

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -165,6 +165,7 @@ const Contact = () => {
               <div className="w-full">
                 <button
                   type='submit'
+                  onClick={handleSend}
                   className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
                 >
                   Send Message
