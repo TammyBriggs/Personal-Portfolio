21e8d09043a5601c550b3f48e90359c3a0887b8c
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-09-24 22:29:21 +0200

    Fi-Menu Bar Fixed

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
@@ -9,35 +9,52 @@ import { navLinksdata } from '../../constants';
 const Navbar = () => {
 
   const [showMenu, setShowMenu]=useState(false)
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    // Update the isLargeScreen state when the window is resized
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 767);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-center items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
-        <ul className="mdl:inline-flex items-center gap-6 lg:gap-10 ">
-          {navLinksdata.map(({ _id, title, link }) => (
-            <li
-              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
-              key={_id}
-            >
-              <Link
-                activeClass="active"
-                to={link}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
+        {isLargeScreen ? ( // Render the menu items for large screens
+          <ul className="mdl:inline-flex items-center gap-6 lg:gap-10">
+            {navLinksdata.map(({ _id, title, link }) => (
+              <li
+                className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+                key={_id}
               >
-                {title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <span
-          onClick={() => setShowMenu(!showMenu)}
-          className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
-        >
-          <FiMenu />
-        </span>
+                <Link
+                  activeClass="active"
+                  to={link}
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  {title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          // Render the menu icon for small screens
+          <span
+            onClick={() => setShowMenu(!showMenu)}
+            className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
+          >
+            <FiMenu />
+          </span>
+        )}
         {showMenu && (
           <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
             <div className="flex flex-col gap-8 py-2 relative">
